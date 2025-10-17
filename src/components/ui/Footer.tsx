a5df79cdc1dad10e04f38caefddc48903970c03f
Héctor <horia@freepik.com>
2025-10-17 14:14:35 +0200

    first commit

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="container-px mx-auto py-10 text-md ">
+    <footer className="container-px py-10 text-md bg-accent text-white">
       © {new Date().getFullYear()} Grain Studio • All rights reserved
     </footer>
   )
